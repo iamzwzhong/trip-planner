b3cc02c96441caf6afcbffbc73aa7cb225cd0ca2
iamzwzhong <zhiwzhong@gmail.com>
2023-10-18 18:00:15 -0700

    Add Flowbite & Tailwind CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
+	import '../app.postcss';
 	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
+		<Header />
 		<slot />
 	</main>
 
