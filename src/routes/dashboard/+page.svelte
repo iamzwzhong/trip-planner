76deba98b0c6a155b581774939826aeb0723ed66
iamzwzhong <zhiwzhong@gmail.com>
2023-10-29 02:54:36 -0700

    small change to dashboard format

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -68,14 +68,76 @@
 			{/if}
 		</div>
 	</section>
+	<div class="flex gap-5">
+		<div class="flex flex-col max-w-xl gap-3">
+			<Heading tag="h2">Personal Trip Dashboard</Heading>
+			<p class="text-lg text-gray-500 dark:text-gray-400 mb-2">
+				Welcome to your personal dashboard page. Here, you'll be able to access a summary view of
+				your trip. View everything that you would need to know about your trip at a quick glance
+				here.
+			</p>
+			<p class="text-lg text-gray-500 dark:text-gray-400 mb-2">
+				Get started by selecting a trip in the top-right section of the dashboard. You must join a
+				trip before you will be able to see any in the dropdown.
+			</p>
+			<ul
+				class="list-none list-inside space-y-4 self-stretch font-medium text-gray-900 dark:text-white border-t dark:border-gray-700 pt-8"
+			>
+				<li class="flex items-center gap-2">
+					<svg
+						class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
+						width="14"
+						height="10"
+						viewBox="0 0 14 10"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
+							stroke="currentColor"
+						/></svg
+					>
+					View upcoming events that you follow
+				</li>
+				<li class="flex items-center gap-2">
+					<svg
+						class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
+						width="14"
+						height="10"
+						viewBox="0 0 14 10"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
+							stroke="currentColor"
+						/></svg
+					>
+					View other events during your trip that you may be interested in
+				</li>
+				<li class="flex items-center gap-2">
+					<svg
+						class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
+						width="14"
+						height="10"
+						viewBox="0 0 14 10"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
+							stroke="currentColor"
+						/></svg
+					>
+					View your profiles of the friends you are traveling with
+				</li>
+			</ul>
+		</div>
+		<img
+			src={selected ? selected.photo : example_dashboard_img}
+			alt={selected ? selected.tripName : 'Example Dashboard Image'}
+			class="rounded-md bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300"
+			style="height: 479px; width:896px;"
+		/>
+	</div>
 	{#if selected}
-		<section>
-			<div class="flex flex-col gap-5 items-center w-full max-w-7xl mx-auto">
-				<div class="my-6" style="width: 1000px; height: 600px">
-					<img src={selected.photo} alt="" class="rounded-3xl w-full h-full object-cover" />
-				</div>
-			</div>
-		</section>
 		<section class="flex flex-row gap-28">
 			<div class="flex flex-col">
 				<Heading tag="h2" class="pb-6">Upcoming Events</Heading>
@@ -90,7 +152,11 @@
 							<div class="flex gap-6">
 								<div>
 									<div class="mb-6" style="width: 450px; height: 300px">
-										<img src={event.photo} alt="" class="rounded-3xl w-full h-full object-cover" />
+										<img
+											src={event.photo}
+											alt={event.eventName + ' photo'}
+											class="rounded-3xl w-full h-full object-cover"
+										/>
 									</div>
 									<div class="ml-5">
 										<Heading
@@ -136,7 +202,7 @@
 								</h4>
 								<img
 									src={avatar}
-									alt=""
+									alt={username + ' avatar'}
 									class="rounded-full bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 w-8 h-8 sm:w-12 sm:h-12 lg:w-16 lg:h-16"
 								/>
 							</div>
@@ -188,75 +254,6 @@
 				</div>
 			</div>
 		</section>
-	{:else}
-		<div class="flex gap-5">
-			<div class="flex flex-col max-w-xl gap-3">
-				<Heading tag="h2">Personal Trip Dashboard</Heading>
-				<p class="text-lg text-gray-500 dark:text-gray-400 mb-2">
-					Welcome to your personal dashboard page. Here, you'll be able to access a summary view of
-					your trip. View everything that you would need to know about your trip at a quick glance
-					here.
-				</p>
-				<p class="text-lg text-gray-500 dark:text-gray-400 mb-2">
-					Get started by selecting a trip in the top-right section of the dashboard. You must join a
-					trip before you will be able to see any in the dropdown.
-				</p>
-				<ul
-					class="list-none list-inside space-y-4 self-stretch font-medium text-gray-900 dark:text-white border-t dark:border-gray-700 pt-8"
-				>
-					<li class="flex items-center gap-2">
-						<svg
-							class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
-							width="14"
-							height="10"
-							viewBox="0 0 14 10"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
-								stroke="currentColor"
-							/></svg
-						>
-						View upcoming events that you follow
-					</li>
-					<li class="flex items-center gap-2">
-						<svg
-							class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
-							width="14"
-							height="10"
-							viewBox="0 0 14 10"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
-								stroke="currentColor"
-							/></svg
-						>
-						View other events during your trip that you may be interested in
-					</li>
-					<li class="flex items-center gap-2">
-						<svg
-							class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
-							width="14"
-							height="10"
-							viewBox="0 0 14 10"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
-								stroke="currentColor"
-							/></svg
-						>
-						View your profiles of the friends you are traveling with
-					</li>
-				</ul>
-			</div>
-			<img
-				src={example_dashboard_img}
-				alt=""
-				class="rounded-md bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 max-w-4xl"
-			/>
-		</div>
 	{/if}
 </main>
 
