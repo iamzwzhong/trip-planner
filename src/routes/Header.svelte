d4480db5ad6407d6514754c9d7d9bb31765b7689
iamzwzhong <zhiwzhong@gmail.com>
2023-11-03 21:56:13 -0700

    Make dashboard page responsive

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -20,7 +20,7 @@
 <div class="sticky top-0 z-40 flex-none mx-auto w-full bg-white dark:bg-gray-900">
 	<Navbar rounded color="form">
 		<NavBrand href="/">
-			<img src={logo} class="mx-3 h-6 sm:h-9" alt="Flowbite Logo" />
+			<img src={logo} class="mx-3 h-6 sm:h-9" alt="Trip Planner Logo" />
 			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
 				>Trip Planner</span
 			>
@@ -31,15 +31,6 @@
 			<NavLi href="/sverdle">Sverdle</NavLi>
 		</NavUl>
 		<div class="flex ml-auto">
-			<Button
-				color="none"
-				data-collapse-toggle="mobile-menu-3"
-				aria-controls="mobile-menu-3"
-				aria-expanded="false"
-				class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
-			>
-				<SearchOutline class="w-5 h-5" />
-			</Button>
 			<div class="hidden relative md:block">
 				<div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
 					<SearchOutline class="w-4 h-4" />
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -50,206 +50,219 @@
 	let index = 0;
 </script>
 
-<main class="flex flex-col items-center gap-16">
-	<section class="flex pt-16 gap-x-96">
-		<div class="flex flex-row mx-auto">
-			<Avatar src={data.users[0].avatar} class="w-52 h-52" />
-			<div style="width:300px;">
-				<Heading tag="h2">{data.users[0].username}</Heading>
+<main>
+	<section>
+		<div
+			class="w-full m-auto p-5 flex flex-col gap-5 text-center lg:flex-row lg:flex-wrap items-center lg:justify-around xl:justify-evenly xl:max-w-screen-3xl"
+		>
+			<div
+				class="flex flex-col items-center w-full gap-2 lg:w-7/12 lg:flex-row lg:justify-center lg:items-start"
+			>
+				<Avatar src={data.users[0].avatar} class="h-300 w-300 max-w-sm lg:h-52 lg:w-52" />
+				<Heading tag="h2" class="w-max text-center tracking-tighter"
+					>{data.users[0].username}</Heading
+				>
+			</div>
+			<div
+				class="flex flex-col items-center gap-3 sm:max-w-3xl lg:order-2 lg:max-w-4xl 2xl:max-w-xl 2xl:items-start"
+			>
+				<Heading tag="h3" class="w-max tracking-tight">My Trip Dashboard</Heading>
+				<p class="text-lg text-gray-500 dark:text-gray-400 text-justify">
+					Welcome to your personal dashboard page. Here, you'll be able to access a summary view of
+					your trip. View everything that you would need to know about your trip at a quick glance
+					here.
+				</p>
+				<p class="text-lg text-gray-500 dark:text-gray-400 text-justify">
+					Get started by selecting a trip in the top-right section of the dashboard. You must join a
+					trip before you will be able to see any in the dropdown.
+				</p>
+				<ul
+					class="list-none list-inside space-y-4 text-left self-stretch font-medium text-gray-900 dark:text-white border-y dark:border-gray-700 py-8 lg:order-2"
+				>
+					<li class="flex items-center gap-2">
+						<svg
+							class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
+							width="14"
+							height="10"
+							viewBox="0 0 14 10"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
+								stroke="currentColor"
+							/></svg
+						>
+						View upcoming events that you follow
+					</li>
+					<li class="flex items-center gap-2">
+						<svg
+							class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
+							width="14"
+							height="10"
+							viewBox="0 0 14 10"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
+								stroke="currentColor"
+							/></svg
+						>
+						View other events during your trip that you may be interested in
+					</li>
+					<li class="flex items-center gap-2">
+						<svg
+							class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
+							width="14"
+							height="10"
+							viewBox="0 0 14 10"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
+								stroke="currentColor"
+							/></svg
+						>
+						View your profiles of the friends you are traveling with
+					</li>
+				</ul>
+			</div>
+			<img
+				src={selected ? selected.photo : example_dashboard_img}
+				alt={selected ? selected.tripName : 'Example Dashboard Image'}
+				class="rounded-md bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 w-full sm:w-auto sm:h-96 lg:order-2 dashboard-image"
+			/>
+			<div class="flex flex-col max-w-xs mx-auto gap-6 lg:order-1">
+				{#if selected}
+					<Heading tag="h3" class="tracking-tighter"
+						>{daysBetween(today, selected.startDate)} days until this trip</Heading
+					>
+				{:else}
+					<Heading tag="h3" class="tracking-tighter">Select a Trip</Heading>
+				{/if}
+				<Select class="mt-2" items={trips} bind:value={selected} />
 			</div>
 		</div>
-		<div class="flex flex-col max-w-sm mx-auto gap-6">
-			<Heading tag="h2">Select a Trip</Heading>
-			<Select class="mt-2" items={trips} bind:value={selected} />
-			{#if selected}
-				<Heading tag="h6" class="text-center"
-					>{daysBetween(today, selected.startDate)} days until this trip</Heading
-				>
-			{/if}
-		</div>
 	</section>
-	<div class="flex gap-5">
-		<div class="flex flex-col max-w-xl gap-3">
-			<Heading tag="h2">Personal Trip Dashboard</Heading>
-			<p class="text-lg text-gray-500 dark:text-gray-400 mb-2">
-				Welcome to your personal dashboard page. Here, you'll be able to access a summary view of
-				your trip. View everything that you would need to know about your trip at a quick glance
-				here.
-			</p>
-			<p class="text-lg text-gray-500 dark:text-gray-400 mb-2">
-				Get started by selecting a trip in the top-right section of the dashboard. You must join a
-				trip before you will be able to see any in the dropdown.
-			</p>
-			<ul
-				class="list-none list-inside space-y-4 self-stretch font-medium text-gray-900 dark:text-white border-t dark:border-gray-700 pt-8"
-			>
-				<li class="flex items-center gap-2">
-					<svg
-						class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
-						width="14"
-						height="10"
-						viewBox="0 0 14 10"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
-							stroke="currentColor"
-						/></svg
-					>
-					View upcoming events that you follow
-				</li>
-				<li class="flex items-center gap-2">
-					<svg
-						class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
-						width="14"
-						height="10"
-						viewBox="0 0 14 10"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
-							stroke="currentColor"
-						/></svg
-					>
-					View other events during your trip that you may be interested in
-				</li>
-				<li class="flex items-center gap-2">
-					<svg
-						class="w-5 h-5 p-1 rounded-full bg-primary-100 dark:bg-gray-700 text-primary-700"
-						width="14"
-						height="10"
-						viewBox="0 0 14 10"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							d="M4.99992 8.29289L4.64637 7.93934L1.35697 4.64995C1.26295 4.56044 1.13776 4.51096 1.00786 4.51209C0.876764 4.51323 0.751358 4.56582 0.658654 4.65852C0.56595 4.75122 0.513365 4.87663 0.512226 5.00773C0.511097 5.13763 0.560568 5.26282 0.650079 5.35684L4.64642 9.35318C4.64644 9.3532 4.64645 9.35322 4.64647 9.35323C4.74023 9.44693 4.86736 9.49957 4.99992 9.49957C5.13248 9.49957 5.25961 9.44693 5.35337 9.35323L4.99992 8.29289ZM4.99992 8.29289L5.35347 7.93934L12.6464 0.646393C12.6464 0.646376 12.6465 0.646358 12.6465 0.64634C12.7402 0.552638 12.8674 0.5 12.9999 0.5C13.1325 0.5 13.2597 0.552658 13.3534 0.646393C13.4471 0.740146 13.4997 0.867253 13.4997 0.999786C13.4997 1.13234 13.4471 1.25947 13.3534 1.35323C13.3533 1.35325 13.3533 1.35327 13.3533 1.35329L5.35342 9.35318L4.99992 8.29289Z"
-							stroke="currentColor"
-						/></svg
-					>
-					View your profiles of the friends you are traveling with
-				</li>
-			</ul>
-		</div>
-		<img
-			src={selected ? selected.photo : example_dashboard_img}
-			alt={selected ? selected.tripName : 'Example Dashboard Image'}
-			class="rounded-md bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300"
-			style="height: 479px; width:896px;"
-		/>
-	</div>
 	{#if selected}
-		<section class="flex flex-row gap-28">
-			<div class="flex flex-col">
-				<Heading tag="h2" class="pb-6">Upcoming Events</Heading>
-				<Timeline order="vertical">
-					{#each myEventsData as event}
-						<TimelineItem>
-							<svelte:fragment slot="icon">
-								<span
-									class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-8 ring-white dark:ring-gray-800 dark:bg-gray-700"
-								/>
-							</svelte:fragment>
-							<div class="flex gap-6">
-								<div>
-									<div class="mb-6" style="width: 450px; height: 300px">
+		<section>
+			<div
+				class="w-full m-auto flex flex-col p-5 gap-5 text-center items-center justify-center lg:flex-row xl:max-w-screen-xl 2xl:items-start"
+			>
+				<div class="lg:w-1/2 items-center flex-col flex">
+					<Heading tag="h2" class="pb-6">Upcoming Events</Heading>
+					<Timeline order="vertical">
+						{#each myEventsData as event}
+							<TimelineItem>
+								<svelte:fragment slot="icon">
+									<span
+										class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-8 ring-white dark:ring-gray-800 dark:bg-gray-700"
+									/>
+								</svelte:fragment>
+								<div
+									class="flex flex-col items-center gap-2 max-w-xl lg:flex-row lg:items-start lg:gap-4 2xl:max-w-7xl"
+								>
+									<div class="flex flex-col items-center lg:items-start">
 										<img
 											src={event.photo}
 											alt={event.eventName + ' photo'}
-											class="rounded-3xl w-full h-full object-cover"
+											class="rounded-3xl object-cover h-60 w-full mb-2 lg:mb-6 lg:h-80 upcoming-events-image"
 										/>
-									</div>
-									<div class="ml-5">
 										<Heading
 											tag="h3"
-											class="rounded-full text-xl font-bold text-white bg-red-500 py-1 px-2"
-											style="width: fit-content">{event.eventTag}</Heading
+											class="rounded-full text-xl font-bold text-white bg-red-500 py-1 px-2 w-fit"
+											>{event.eventTag}</Heading
 										>
 									</div>
+									<div class="lg:max-w-xs lg:text-left">
+										<h4 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+											{event.eventName}
+										</h4>
+										<h6 class="font-bold text-gray-900 dark:text-white">
+											{new Date(event.startTime).toDateString()}
+										</h6>
+										<h6 class="font-bold text-gray-900 dark:text-white">
+											{new Date(event.startTime).toLocaleTimeString() +
+												' - ' +
+												new Date(event.endTime).toLocaleTimeString()}
+										</h6>
+										<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight max-w-sm">
+											{event.description}
+										</p>
+									</div>
 								</div>
-								<div class="max-w-xs">
-									<h4 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-										{event.eventName}
-									</h4>
-									<h6 class="font-bold text-gray-900 dark:text-white">
-										{new Date(event.startTime).toDateString()}
-									</h6>
-									<h6 class="mb-2 font-bold text-gray-900 dark:text-white">
-										{new Date(event.startTime).toLocaleTimeString() +
-											' - ' +
-											new Date(event.endTime).toLocaleTimeString()}
-									</h6>
-									<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-										{event.description}
-									</p>
-								</div>
-							</div>
-						</TimelineItem>
-					{/each}
-				</Timeline>
-			</div>
-			<div class="flex flex-col items-left gap-6">
-				<div class="flex flex-col">
-					<Heading tag="h2" class="pb-6">Travel Friends</Heading>
-					<div
-						class="flex flex-col overflow-y-scroll travel-friends-scrollbar"
-						style="height: 300px;"
-						dir="rtl"
-					>
-						{#each data.users as { avatar, username }}
-							<div class="flex mr-auto gap-5 mb-3 ml-5">
-								<h4 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white my-auto">
-									{username}
-								</h4>
-								<img
-									src={avatar}
-									alt={username + ' avatar'}
-									class="rounded-full bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 w-8 h-8 sm:w-12 sm:h-12 lg:w-16 lg:h-16"
-								/>
-							</div>
+							</TimelineItem>
 						{/each}
-					</div>
+					</Timeline>
 				</div>
-				<Heading tag="h2">Other Events</Heading>
-				<div class="max-w-xl space-y-4">
-					<Carousel {images} let:Indicators let:Controls bind:index transition={null}>
-						<Indicators />
-						<Controls />
-					</Carousel>
-					<Thumbnails
-						class="bg-transparent gap-3"
-						let:Thumbnail
-						let:image
-						let:selected
-						{images}
-						bind:index
-					>
-						<Thumbnail
-							{...image}
-							{selected}
-							class="rounded-md shadow-xl hover:outline hover:outline-primary-500 h-20"
-							activeClass="outline outline-primary-400"
-						/>
-					</Thumbnails>
-					<div class="flex flex-col items-center text-center">
-						<Heading
-							tag="h3"
-							class="rounded-full text-xl font-bold text-white bg-red-500 py-1 px-2"
-							style="width: fit-content">{data.events[0].eventTag}</Heading
+				<div class="lg:w-1/2 self-stretch justify-around flex-col flex">
+					<div class="flex flex-col gap-5 text-center items-center">
+						<Heading tag="h2" class="pb-6">Travel Friends</Heading>
+						<div
+							class="flex flex-col overflow-y-scroll travel-friends-scrollbar max-h-72"
+							dir="rtl"
 						>
-						<Heading tag="h4" class="tracking-tight">
-							{data.events[0].eventName}
-						</Heading>
-						<Heading tag="h6">
-							{new Date(data.events[0].startTime).toDateString()}
-						</Heading>
-						<Heading tag="h6" class="mb-2">
-							{new Date(data.events[0].startTime).toLocaleTimeString() +
-								' - ' +
-								new Date(data.events[0].endTime).toLocaleTimeString()}
-						</Heading>
+							{#each data.users as { avatar, username }}
+								<div class="flex mr-auto gap-5 mb-3 ml-5">
+									<h4
+										class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white my-auto"
+									>
+										{username}
+									</h4>
+									<img
+										src={avatar}
+										alt={username + ' avatar'}
+										class="rounded-full bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 w-8 h-8 sm:w-12 sm:h-12"
+									/>
+								</div>
+							{/each}
+						</div>
 					</div>
-					<div class="rounded bg-gray-300 dark:bg-gray-700 dark:text-white p-2 my-2 text-justify">
-						{images[index].alt}
+					<div
+						class="w-full max-w-sm m-auto p-5 flex flex-col gap-5 text-center items-center sm:max-w-lg"
+					>
+						<Heading tag="h2">Other Events</Heading>
+						<div class="max-w-xl space-y-4">
+							<Carousel {images} let:Indicators let:Controls bind:index transition={null}>
+								<Indicators />
+								<Controls />
+							</Carousel>
+							<Thumbnails
+								class="bg-transparent gap-3"
+								let:Thumbnail
+								let:image
+								let:selected
+								{images}
+								bind:index
+							>
+								<Thumbnail
+									{...image}
+									{selected}
+									class="rounded-md shadow-xl hover:outline hover:outline-primary-500 h-20"
+									activeClass="outline outline-primary-400"
+								/>
+							</Thumbnails>
+							<div class="flex flex-col items-center text-center">
+								<Heading
+									tag="h3"
+									class="rounded-full text-xl font-bold text-white bg-red-500 py-1 px-2"
+									style="width: fit-content">{data.events[0].eventTag}</Heading
+								>
+								<Heading tag="h4" class="tracking-tight">
+									{data.events[0].eventName}
+								</Heading>
+								<Heading tag="h6">
+									{new Date(data.events[0].startTime).toDateString()}
+								</Heading>
+								<Heading tag="h6">
+									{new Date(data.events[0].startTime).toLocaleTimeString() +
+										' - ' +
+										new Date(data.events[0].endTime).toLocaleTimeString()}
+								</Heading>
+							</div>
+							<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+								{images[index].alt}
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -276,5 +289,19 @@
 
 	.travel-friends-scrollbar::-webkit-scrollbar-thumb:hover {
 		background: #c0a0b9;
+	}
+
+	@media (min-width: 1024px) {
+		.dashboard-image {
+			height: 479px;
+			width: 896px;
+		}
+	}
+
+	@media (min-width: 1536px) {
+		.upcoming-events-image {
+			width: 650px;
+			height: 250px;
+		}
 	}
 </style>
