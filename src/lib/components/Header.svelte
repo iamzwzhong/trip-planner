de1bed0ffee7c0dcb56ce2ec1e92fc2a478b2ece
iamzwzhong <zhiwzhong@gmail.com>
2024-03-23 02:17:40 -0700

    Create home page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,15 +4,15 @@
 	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<div class="sticky top-0 mx-auto w-full flex-none bg-white dark:bg-gray-900">
-	<Navbar>
-		<NavBrand href="/">
+<div class="sticky top-0 w-full flex-none">
+	<Navbar class="bg-gray-300">
+		<NavBrand href="/" class="sm:min-w-40">
 			<img src={logo} class="me-3 h-6 sm:h-9" alt="Trip Planner Logo" />
 			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
 				>Trip Planner</span
 			>
 		</NavBrand>
-		<div class="flex md:order-2">
+		<div class="flex justify-end sm:min-w-40 md:order-2">
 			<NavHamburger />
 			<DarkMode />
 		</div>
